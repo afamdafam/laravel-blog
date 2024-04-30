2fec3b156ef4c1f9fbdce6ea3841d0ce9bf802eb
afamdafam <adam.hp31@gmail.com>
2024-05-01 02:25:43 +0700

    added about page

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
 Route::get('/dashboard',[DashboardController::class,'index']);
