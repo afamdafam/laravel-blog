cd7cd467eca6ab0700e256ea813491262351d3c9
afamdafam <adam.hp31@gmail.com>
2024-05-06 23:39:29 +0700

    updated dashboard

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
+use App\Models\Posts;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('back.dashboard.index');
+        return view('back.dashboard.index', [
+            'total_posts' => Posts::count(),
+            'total_views' =>  Posts::sum('views'),
+            'latest_posts' => Posts::with('User')->whereStatus(1)->latest()->take(5)->get(),
+            'popular_posts' => Posts::with('User')->whereStatus(1)->orderBy('views')->take(5)->get()
+        ]);
     
 }
 }
--- resources/views/back/dashboard/index.blade.php
+++ resources/views/back/dashboard/index.blade.php
@@ -8,147 +8,94 @@
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group me-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-          </div>
-          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-            <span data-feather="calendar" class="align-text-bottom"></span>
-            This week
-          </button>
+      </div>
+
+      <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+          <h1 class="display-5 fw-bold text-center">Hello, Admin !</h1>
         </div>
       </div>
 
-      <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+      <div class="row text-center">
+        <div class="col-6">
+          <div class="card text-white mb-3" style="max-width: 100%;">
+            <div class="card-header bg-success">Total Posts</div>
+            <div class="card-body">
+              <h5 class="card-title text-black">{{ $total_posts}} posts</h5>
+            </div>
+          </div>
+        </div>
 
-      <h2>Section title</h2>
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
+        <div class="col-6">
+          <div class="card text-white mb-3" style="max-width: 100%;">
+            <div class="card-header bg-primary">Total Views</div>
+            <div class="card-body">
+              <h5 class="card-title text-black">{{ $total_views}} views</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row text-center">
+        <div class="col-6">
+          <h4>Latest Posts</h4>
+          <div class="table-container" style="overflow-y: auto;
+          overflow-x: auto;">
+          <table class="table table-bordered table-striped" >
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
+              <th>No.</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Created At</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>random</td>
-              <td>data</td>
-              <td>placeholder</td>
-              <td>text</td>
-            </tr>
-            <tr>
-              <td>1,002</td>
-              <td>placeholder</td>
-              <td>irrelevant</td>
-              <td>visual</td>
-              <td>layout</td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>data</td>
-              <td>rich</td>
-              <td>dashboard</td>
-              <td>tabular</td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>information</td>
-              <td>placeholder</td>
-              <td>illustrative</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,004</td>
-              <td>text</td>
-              <td>random</td>
-              <td>layout</td>
-              <td>dashboard</td>
-            </tr>
-            <tr>
-              <td>1,005</td>
-              <td>dashboard</td>
-              <td>irrelevant</td>
-              <td>text</td>
-              <td>placeholder</td>
-            </tr>
-            <tr>
-              <td>1,006</td>
-              <td>dashboard</td>
-              <td>illustrative</td>
-              <td>rich</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,007</td>
-              <td>placeholder</td>
-              <td>tabular</td>
-              <td>information</td>
-              <td>irrelevant</td>
-            </tr>
-            <tr>
-              <td>1,008</td>
-              <td>random</td>
-              <td>data</td>
-              <td>placeholder</td>
-              <td>text</td>
-            </tr>
-            <tr>
-              <td>1,009</td>
-              <td>placeholder</td>
-              <td>irrelevant</td>
-              <td>visual</td>
-              <td>layout</td>
-            </tr>
-            <tr>
-              <td>1,010</td>
-              <td>data</td>
-              <td>rich</td>
-              <td>dashboard</td>
-              <td>tabular</td>
-            </tr>
-            <tr>
-              <td>1,011</td>
-              <td>information</td>
-              <td>placeholder</td>
-              <td>illustrative</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,012</td>
-              <td>text</td>
-              <td>placeholder</td>
-              <td>layout</td>
-              <td>dashboard</td>
-            </tr>
-            <tr>
-              <td>1,013</td>
-              <td>dashboard</td>
-              <td>irrelevant</td>
-              <td>text</td>
-              <td>visual</td>
-            </tr>
-            <tr>
-              <td>1,014</td>
-              <td>dashboard</td>
-              <td>illustrative</td>
-              <td>rich</td>
-              <td>data</td>
-            </tr>
-            <tr>
-              <td>1,015</td>
-              <td>random</td>
-              <td>tabular</td>
-              <td>information</td>
-              <td>text</td>
-            </tr>
-          </tbody>
+            @foreach ($latest_posts as $post)
+              <tr>
+                <td>{{$loop->iteration}}</td>
+                <td>{{$post->title}}</td>
+                <td>{{$post->User->name}}</td>
+                <td>{{$post->created_at->format('d M Y')}}</td>
+                <td><a href="{{ url("post/$post->id") }}" class="btn btn-primary">View</a></td>
+              </tr>
+            @endforeach
+          </tbody>      
         </table>
+        </div>
+        </div>
+
+        <div class="col-6">
+          <h4>Popular Posts</h4>
+          <div class="table-container" style="overflow-y: auto;
+          overflow-x: auto;">
+          <table class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Views</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($popular_posts as $post)
+              <tr>
+                <td>{{$loop->iteration}}</td>
+                <td>{{$post->title}}</td>
+                <td>{{$post->User->name}}</td>
+                <td>{{$post->views}}</td>
+                <td><a href="{{ url("post/$post->id") }}" class="btn btn-primary">View</a></td>
+              </tr>
+            @endforeach
+          </tbody>      
+        </table>
+        </div>
+        </div>
+      </div>
+
       </div>
     </main>
   </div>
